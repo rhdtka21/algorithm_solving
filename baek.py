e231b0e8c2207a05c1d84028b918b28e05254639
rhdtka21 <rhdtka21@gmail.com>
2020-04-22 22:47:48 +0900

    initail commit

---
+++ baek.py
@@ -0,0 +1,18 @@
+T = int(input())
+for t in range(1, T+1):
+    
+    N = int(input())
+    tasks = []
+    for _ in range(N):
+        tasks.append(list(map(int, input().split())))
+    tasks.sort(key = lambda x : (x[1], x[0]))
+    
+    ans = 1
+    end = tasks[0][1]
+    
+    for i in range(1, N):
+        if tasks[i][0] >= end:
+            end = tasks[i][1]
+            ans += 10
+
+    print('#%d %d' % (t, ans))
