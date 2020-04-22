c5bb0021d4cfde8547a4a71825e92c5f72fbb6b7
rhdtka21 <rhdtka21@gmail.com>
2020-04-22 23:11:55 +0900

    [파이썬 | BOJ | 2580] 스도쿠

--- BOJ.py
+++ BOJ.py
@@ -1,20 +1,52 @@
-# [파이썬 | BOJ | 9084] 동전
+# [파이썬 | BOJ | 2580] 스도쿠
 
 import sys
 read = sys.stdin.readline
  
-T = int(read())
+def myPrint(arr):
+    for i in arr:
+        for j in i:
+            print(j, end=' ')
+        print()
  
-while T:
-    N = int(read())
-    coin = list(map(int, read().split()))
-    M = int(read())
-    d = [0 for _ in range(M+1)]
-    d[0] = 1
+def check(nowI, nowJ, k):
+    if k in sudoku[nowI]:
+        return False
+    
+    for i in range(9):
+        if sudoku[i][nowJ] == k:
+            return False
+    
+    tempI, tempJ = nowI//3 * 3, nowJ//3 * 3
+    for i in range(3):
+        for j in range(3):
+            if sudoku[tempI+i][tempJ+j] == k:
+                return False
  
-    for i in range(N):
-        for j in range(coin[i], M+1):
-            d[j] += d[j-coin[i]]
+    return True
  
-    print(d[M])
-    T -= 1
+def DFS(idx):
+    if idx == len(blank):
+        myPrint(sudoku)
+        sys.exit(0)
+        return
+    else:
+        for k in range(1, 10):
+            nowI, nowJ = blank[idx][0], blank[idx][1]
+            if check(nowI, nowJ, k):
+                sudoku[nowI][nowJ] = k
+                DFS(idx+1)
+                sudoku[nowI][nowJ] = 0
+ 
+sudoku = []
+blank = []
+ 
+for i in range(9):
+    sudoku.append(list(map(int, read().split())))
+ 
+for i in range(9):
+    for j in range(9):
+        if sudoku[i][j] == 0:
+            blank.append([i,j])
+ 
+DFS(0)
