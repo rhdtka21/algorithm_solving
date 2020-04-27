979e3679dab59fc18d69eb4b5f628d0c0049be65
rhdtka21 <rhdtka21@gmail.com>
2020-04-27 17:55:23 +0900

    [파이썬 | BOJ | 2580] 스도쿠

--- BOJ2580.py
+++ BOJ2580.py
@@ -1,30 +1,30 @@
-# [∆ƒ¿ÃΩ„ | BOJ | 2580] Ω∫µµƒÌ
+# [ÌååÏù¥Ïç¨ | BOJ | 2580] Ïä§ÎèÑÏø†
 
 import sys
 read = sys.stdin.readline
- 
+
 def myPrint(arr):
     for i in arr:
         for j in i:
             print(j, end=' ')
         print()
- 
+
 def check(nowI, nowJ, k):
     if k in sudoku[nowI]:
         return False
-    
+
     for i in range(9):
         if sudoku[i][nowJ] == k:
             return False
-    
+
     tempI, tempJ = nowI//3 * 3, nowJ//3 * 3
     for i in range(3):
         for j in range(3):
             if sudoku[tempI+i][tempJ+j] == k:
                 return False
- 
+
     return True
- 
+
 def DFS(idx):
     if idx == len(blank):
         myPrint(sudoku)
@@ -37,16 +37,16 @@ def DFS(idx):
                 sudoku[nowI][nowJ] = k
                 DFS(idx+1)
                 sudoku[nowI][nowJ] = 0
- 
+
 sudoku = []
 blank = []
- 
+
 for i in range(9):
     sudoku.append(list(map(int, read().split())))
- 
+
 for i in range(9):
     for j in range(9):
         if sudoku[i][j] == 0:
             blank.append([i,j])
- 
-DFS(0)
+
+DFS(0) 
