f7eb026f2b1efd94a6a23313aea7db24256365b5
rhdtka21 <rhdtka21@gmail.com>
2020-05-01 02:02:29 +0900

    [파이썬 | BOJ | 17825] 주사위 윷놀이

--- BOJ17825.py
+++ BOJ17825.py
@@ -1,170 +1,118 @@
 # [파이썬 | BOJ | 17825] 주사위 윷놀이
 import sys
 read = sys.stdin.readline
-maxval = sys.maxsize * -1
-BRANCH = [[], [], [], []]
-BRANCH[0] = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]        
-BRANCH[1] = [10, 13, 16, 19, 25, 30, 35, 40]                                                 
-BRANCH[2] = [20, 22, 24, 25, 30, 35, 40]
-BRANCH[3] = [30, 28, 27, 26, 25, 30, 35, 40]   
+maxAns = sys.maxsize * -1
+BRANCH = [[], [], [], [], []]                                                           #20  21
+BRANCH[0] = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 0]        
+BRANCH[1] = [10, 13, 16, 19]                                                 
+BRANCH[2] = [20, 22, 24]
+BRANCH[3] = [30, 28, 27, 26]
+BRANCH[4] = [25, 30, 35, 40, 0]
 
-def solve(horses, cnt, depth):
-    global maxval 
-
-    if depth == 10:
-        if maxval < cnt:
-            maxval = cnt
+def solve(cnt, depth):
+    global maxAns
+    #print(locInfo, cnt, depth)
+    if depth == len(diceNum):
+        if maxAns < cnt:
+            maxAns = cnt
+            #print(locInfo, cnt)
         return
 
-    go = diceNum[depth]
+    for horseNum, li in enumerate(locInfo):
+        #현재 위치
+        branch, location = li
+        
+        #이미 도착한 상태면 해당 말을 고를 수 없다.
+        if (branch, location) == (0, 21) or (branch, location) == (4, 4): 
+            continue
+        
+        #주사위 정보를 통한 다음위치
+        nextLocation = location + diceNum[depth]
 
-    # 0번말
-    branch = horses[0][0]
-    index = horses[0][1]
+        #윷말 이동 정리 알고리즘
+        #지금 0번 위치에 있는데, 말의 다음 위치가 분기점 이라면,
+        if branch == 0:
+            #지금 0번 위치에 있는데 말이 종점에 도착한다. (점수를 안준다.)
+            if nextLocation >= 22:
+                #더이상 말이 이동못할것임
+                nextLocation = 21
+                nextBranch = branch
+            #다음 위치가 10번이라면
+            elif BRANCH[branch][nextLocation] == 10:
+                #1번 가지로 빠지고, 거기서 0번 위치로 이동함
+                nextBranch = 1
+                nextLocation = 0
+            #다음 위치가 20번 이라면
+            elif BRANCH[branch][nextLocation] == 20:
+                #2번 가지로 빠지고, 거기서 0번 위치로 이동
+                nextBranch = 2
+                nextLocation = 0
+            #다음 위치가 30번이라면
+            elif BRANCH[branch][nextLocation] == 30:
+                #3번 가지로 빠지고, 거기서 0번 위치로 이동
+                nextBranch = 3
+                nextLocation = 0
+            #종점도, 분기점도 아니라면
+            else:
+                #가지는 그대로
+                nextBranch = branch
+        #지금 1번 위치에 있는데, 
+        elif branch == 1:
+            #말이 25번을 넘어간다면
+            if nextLocation >= 4:
+                nextBranch = 4
+                nextLocation = nextLocation - 4
+            #안넘어 간다면
+            else:
+                nextBranch = branch
+            
 
-    if index == -1:
-        return
+        elif branch == 2:
+            if nextLocation >= 3:
+                nextBranch = 4
+                nextLocation = nextLocation - 3
+            else:
+                nextBranch = branch
+            
 
-    nextIndex = index + go
-    if nextIndex >= len(BRANCH[0]):
-        addVal = 40
-        nextIndex = -1
+        elif branch == 3:
+            if nextLocation >= 4:
+                nextBranch = 4
+                nextLocation = nextLocation - 4
+            else:
+                nextBranch = branch
+            
+        #지금 4번 위치에 있는데
+        elif branch == 4:
+            if nextLocation >= 4:
+                #print(locInfo, cnt)
+                nextBranch = branch
+                nextLocation = 4
+            else:
+                nextBranch = branch
 
-    else:
-        if branch == 0 and BRANCH[branch][nextIndex] == 10:
-            branch = 1
-            nextIndex = 0
+        
+        if (nextBranch, nextLocation) == (0, 21) or (nextBranch, nextLocation) == (4, 4):
+            locInfo[horseNum] = (nextBranch, nextLocation)
+            solve(cnt + BRANCH[nextBranch][nextLocation], depth+1)
+            locInfo[horseNum] = (branch, location)
+            continue
 
-        elif branch == 0 and BRANCH[branch][nextIndex] == 20:
-            branch = 2
-            nextIndex = 0
+        if (nextBranch, nextLocation) in locInfo:
+            continue
+        
+        if (nextBranch, nextLocation) == (0, 20) and (4, 3) in locInfo:
+            continue
+        
+        if (nextBranch, nextLocation) == (4, 3) and (0, 20) in locInfo:
+            continue
 
-        elif branch == 0 and BRANCH[branch][nextIndex] == 30:
-            branch = 3
-            nextIndex = 0
-
-        try:
-            addVal = BRANCH[branch][nextIndex]
-        except IndexError:
-            addVal = 40
-            nextIndex = -1
-    if not (branch, nextIndex) in horses:
-        if branch > 0 and nextIndex in [25, 30, 35, 40]:
-            if horses[1][1] != nextIndex and horses[2][1] != nextIndex and horses[3][1] != nextIndex:
-                solve([ (branch, nextIndex), horses[1], horses[2], horses[3] ], cnt + addVal, depth + 1)
-        else:
-            solve([ (branch, nextIndex), horses[1], horses[2], horses[3] ], cnt + addVal, depth + 1)
-
-    # 1번말
-    branch = horses[1][0]
-    index = horses[1][1]
-
-    if index == -1:
-        return
-
-    nextIndex = index + go
-    if nextIndex >= len(BRANCH[0]):
-        addVal = 40
-        nextIndex = -1
-
-    else:
-        if branch == 0 and BRANCH[branch][nextIndex] == 10:
-            branch = 1
-            nextIndex = 0
-
-        elif branch == 0 and BRANCH[branch][nextIndex] == 20:
-            branch = 2
-            nextIndex = 0
-
-        elif branch == 0 and BRANCH[branch][nextIndex] == 30:
-            branch = 3
-            nextIndex = 0
-
-        try:
-            addVal = BRANCH[branch][nextIndex]
-        except IndexError:
-            addVal = 40
-            nextIndex = -1
-    if not (branch, nextIndex) in horses:
-        if branch > 0 and nextIndex in [25, 30, 35, 40]:
-            if horses[0][1] != nextIndex and horses[2][1] != nextIndex and horses[3][1] != nextIndex:
-                solve([ horses[0], (branch, nextIndex), horses[2], horses[3] ], cnt + addVal, depth + 1)
-        else:
-            solve([ horses[0], (branch, nextIndex), horses[2], horses[3] ], cnt + addVal, depth + 1)
-    # 2번말
-    branch = horses[2][0]
-    index = horses[2][1]
-
-    if index == -1:
-        return
-
-    nextIndex = index + go
-    if nextIndex >= len(BRANCH[0]):
-        addVal = 40
-        nextIndex = -1
-
-    else:
-        if branch == 0 and BRANCH[branch][nextIndex] == 10:
-            branch = 1
-            nextIndex = 0
-
-        elif branch == 0 and BRANCH[branch][nextIndex] == 20:
-            branch = 2
-            nextIndex = 0
-
-        elif branch == 0 and BRANCH[branch][nextIndex] == 30:
-            branch = 3
-            nextIndex = 0
-
-        try:
-            addVal = BRANCH[branch][nextIndex]
-        except IndexError:
-            addVal = 40
-            nextIndex = -1
-    if not (branch, nextIndex) in horses:
-        if branch > 0 and nextIndex in [25, 30, 35, 40]:
-            if horses[0][1] != nextIndex and horses[1][1] != nextIndex and horses[3][1] != nextIndex:
-                solve([ horses[0], horses[1], (branch, nextIndex), horses[3] ], cnt + addVal, depth + 1)
-        else:
-            solve([ horses[0], horses[1], (branch, nextIndex), horses[3] ], cnt + addVal, depth + 1)
-    # 3번말
-    branch = horses[3][0]
-    index = horses[3][1]
-
-    if index == -1:
-        return
-
-    nextIndex = index + go
-    if nextIndex >= len(BRANCH[0]):
-        addVal = 40
-        nextIndex = -1
-
-    else:
-        if branch == 0 and BRANCH[branch][nextIndex] == 10:
-            branch = 1
-            nextIndex = 0
-
-        elif branch == 0 and BRANCH[branch][nextIndex] == 20:
-            branch = 2
-            nextIndex = 0
-
-        elif branch == 0 and BRANCH[branch][nextIndex] == 30:
-            branch = 3
-            nextIndex = 0
-
-        try:
-            addVal = BRANCH[branch][nextIndex]
-        except IndexError:
-            addVal = 40
-            nextIndex = -1
-    if not (branch, nextIndex) in horses:
-        if branch > 0 and nextIndex in [25, 30, 35, 40]:
-            if horses[0][1] != nextIndex and horses[1][1] != nextIndex and horses[2][1] != nextIndex:
-                solve([ horses[0], horses[1], horses[2], (branch, nextIndex)], cnt + addVal, depth + 1)
-        else:
-            solve([ horses[0], horses[1], horses[2], (branch, nextIndex)], cnt + addVal, depth + 1)
+        locInfo[horseNum] = (nextBranch, nextLocation)
+        solve(cnt + BRANCH[nextBranch][nextLocation], depth+1)
+        locInfo[horseNum] = (branch, location)
+            
 
 diceNum = list(map(int, read().split()))
-solve([(0,0), (0,0), (0,0), (0,0)], 0, 0)
-
-print(maxval)
+locInfo = [(0,0), (0,0), (0,0), (0,0)]
+solve(0, 0)
+print(maxAns)
